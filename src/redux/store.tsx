896cb09605f10b4be86085449ab508103a6921be
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-18 21:34:33 +0900

    [feat] hooks.ts 파일 작성

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -11,3 +11,5 @@ export type RootState = ReturnType<typeof store.getState>; // useAppSelector
 export type AppDispatch = typeof store.dispatch; // useAppDispatch
 
 export default store;
+
+// useSelector(리덕스에 작성한 변수 접근 조회), useDispatch(변수 업데이트)
