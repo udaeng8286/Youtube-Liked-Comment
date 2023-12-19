06280e731f7c1012020244fa3a08ce56c2b5d4c8
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-19 21:12:04 +0900

    [feat] import PersistGate, persistStore

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.tsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
