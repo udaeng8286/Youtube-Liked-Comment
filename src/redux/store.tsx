34e94f05215f0d7d8f6bbe9d7900f242f06d8a8c
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-19 21:06:25 +0900

    [feat] persistConfig 작성, 지속 리듀서 명시

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -3,6 +3,13 @@ import videoReducer from "./VideoSlice";
 import commentReducer from "./CommentSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  storage, // localStorage
+  whiteList: ["video", "comment"],
+};
 
 const store = configureStore({
   reducer: {
